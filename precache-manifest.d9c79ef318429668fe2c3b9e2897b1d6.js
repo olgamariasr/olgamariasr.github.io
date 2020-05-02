b8afdbababbc917977c496aa1fcc1013b44d4bb3
olgamariasr <olga.maria.sr@gmail.com>
2020-05-02 12:39:07 +0200

    Updates

---
+++ precache-manifest.d9c79ef318429668fe2c3b9e2897b1d6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fd0d3379582ab07a84c",
+    "url": "/olgamariasr/olgamariasr.github.io/static/css/main.1fc5f5ec.chunk.css"
+  },
+  {
+    "revision": "0fd0d3379582ab07a84c",
+    "url": "/olgamariasr/olgamariasr.github.io/static/js/main.cc4354d5.chunk.js"
+  },
+  {
+    "revision": "32366b6fadcf1c3a7637",
+    "url": "/olgamariasr/olgamariasr.github.io/static/js/runtime~main.4eee8722.js"
+  },
+  {
+    "revision": "446f1cf05baa691bde37",
+    "url": "/olgamariasr/olgamariasr.github.io/static/css/2.a3023c29.chunk.css"
+  },
+  {
+    "revision": "446f1cf05baa691bde37",
+    "url": "/olgamariasr/olgamariasr.github.io/static/js/2.216510d4.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/olgamariasr/olgamariasr.github.io/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/olgamariasr/olgamariasr.github.io/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/olgamariasr/olgamariasr.github.io/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/olgamariasr/olgamariasr.github.io/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/olgamariasr/olgamariasr.github.io/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "3294e188e6d2f3ec0c508f3da2117dbd",
+    "url": "/olgamariasr/olgamariasr.github.io/index.html"
+  }
+];
