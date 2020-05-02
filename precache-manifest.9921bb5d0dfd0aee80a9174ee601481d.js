848145c7b9a9a9e67c20ea0e559c2d21ed306436
olgamariasr <olga.maria.sr@gmail.com>
2020-05-02 12:42:34 +0200

    Updates

--- precache-manifest.9921bb5d0dfd0aee80a9174ee601481d.js
+++ precache-manifest.9921bb5d0dfd0aee80a9174ee601481d.js
@@ -20,16 +20,12 @@ self.__precacheManifest = [
     "url": "/olgamariasr/olgamariasr.github.io/static/js/2.216510d4.chunk.js"
   },
   {
-    "revision": "af7ae505a9eed503f8b8e6982036873e",
-    "url": "/olgamariasr/olgamariasr.github.io/static/media/fontawesome-webfont.af7ae505.woff2"
-  },
-  {
-    "revision": "912ec66d7572ff821749319396470bde",
-    "url": "/olgamariasr/olgamariasr.github.io/static/media/fontawesome-webfont.912ec66d.svg"
-  },
-  {
     "revision": "674f50d287a8c48dc19ba404d20fe713",
     "url": "/olgamariasr/olgamariasr.github.io/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/olgamariasr/olgamariasr.github.io/static/media/fontawesome-webfont.af7ae505.woff2"
   },
   {
     "revision": "fee66e712a8a08eef5805a46892932ad",
@@ -38,6 +34,10 @@ self.__precacheManifest = [
   {
     "revision": "b06871f281fee6b241d60582ae9369b9",
     "url": "/olgamariasr/olgamariasr.github.io/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/olgamariasr/olgamariasr.github.io/static/media/fontawesome-webfont.912ec66d.svg"
   },
   {
     "revision": "3294e188e6d2f3ec0c508f3da2117dbd",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/olgamariasr/olgamariasr.github.io/precache-manifest.fc5dec6678458ced35b7be1661cb7b8c.js"
+  "/olgamariasr/olgamariasr.github.io/precache-manifest.9921bb5d0dfd0aee80a9174ee601481d.js"
 );
 
 workbox.clientsClaim();
